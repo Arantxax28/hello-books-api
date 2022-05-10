85c0159b5d7238e369d10d67498749ec15beef96
Diana <arantxa.codes@gmail.com>
2022-05-09 17:00:15 -0700

    Added author model and other things

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,11 @@
 from os import abort
 from app import db
 from app.models.book import Book
+from app.models.author import Author
 from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+authors_bp = Blueprint("authors_bp", __name__, url_prefix="/books")
 
 def validate_book(book_id):
     try:
@@ -81,4 +83,22 @@ def delete_book(book_id):
     return make_response(jsonify(f"Book #{book.id} successfully deleted"))
 
 
+@authors_bp.route("", methods=["GET"])
+def get_all_authors():
+    authors = Author.query.all()
+    author_response = []
+    for author in authors:
+        author_response.append({
+            "name": author.name
+        })
+    return jsonify(author_response)
 
+@authors_bp.route("", methods=["POST"])
+def create_author():
+    request_body = request.json()
+    new_author = Author(name=request_body["name"]) 
+
+    db.session.add(new_author)
+    db.session.commit()
+
+    return make_response(jsonify(f"Author {new_author.name} successfully created"), 201)
